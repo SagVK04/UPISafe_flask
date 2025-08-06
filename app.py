b35cd58d0586bf31c3527ef80628c2e5397dcd49
Sagnik Metiya <metiyasagnik692@gmail.com>
2025-08-06 17:57:05 +0530

    Merge remote-tracking branch 'origin/master'

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
-import streamlit as st
 
 model = pickle.load(open('fraud_detector.pkl', 'rb'))
 app = Flask(__name__)
 
-
-@app.route('/', methods=['POST'])
+# This is the endpoint that receives data
+@app.route('/predict', methods=['POST'])
 def predict():
-    st.header("Hello!")
     amount = request.form.get('amount')
     time = request.form.get('time')
     date = request.form.get('date')
     platform = request.form.get('platform')
-    type = request.form.get('type')
+    transaction_type = request.form.get('type')
 
-    input_query = np.array([[float(amount), int(time), int(date), int(platform), int(type)]])
+    # Create the input array
+    input_query = np.array([[float(amount), int(time), int(date), int(platform), int(transaction_type)]])
 
+    # Make the prediction
     result = model.predict(input_query)[0]
 
     return jsonify({'Fraud Possibility': str(result)})
+
+if __name__ == "__main__":
+    app.run()
